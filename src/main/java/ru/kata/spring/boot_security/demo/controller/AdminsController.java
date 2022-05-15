2dd2e923b7095fca876c2f6d8d72c8ff1f232595
iliya68rus <iliya97pro@yandex.ru>
2022-05-15 14:14:37 +0300

    добавил сервисный слой

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminsController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminsController.java
@@ -4,31 +4,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import ru.kata.spring.boot_security.demo.dao.UserDAO;
 import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.services.UserService;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminsController {
 
     @Autowired
-    private UserDAO userdao;
+    private UserService userService;
 
     @GetMapping("/users")
     public String showUsers(Model model) {
-        model.addAttribute("users", userdao.getAllUser());
+        model.addAttribute("users", userService.getAllUser());
         return "users/index";
     }
 
     @PostMapping("/users")
     public String createUser(@ModelAttribute("user") User user) {
-        userdao.saveUser(user);
+        userService.saveUser(user);
         return "redirect:/admin/users";
     }
 
     @GetMapping("/users/{id}")
     public String showUsersById(@PathVariable("id") long id, Model model) {
-        model.addAttribute("user", userdao.getUserById(id));
+        model.addAttribute("user", userService.getUserById(id));
         return "users/show";
     }
 
@@ -40,19 +40,19 @@ public class AdminsController {
 
     @GetMapping("/users/{id}/edit")
     public String editUser(@PathVariable("id") long id, Model model) {
-        model.addAttribute("user", userdao.getUserById(id));
+        model.addAttribute("user", userService.getUserById(id));
         return "users/edit";
     }
 
     @PatchMapping("/users/{id}")
     public String editUser(@ModelAttribute("user") User user, @PathVariable("id") long id) {
-        userdao.editUser(id, user);
+        userService.editUser(id, user);
         return "redirect:/admin/users";
     }
 
     @DeleteMapping("/users/{id}")
     public String deleteUser(@PathVariable("id") long id) {
-        userdao.deleteUser(id);
+        userService.deleteUser(id);
         return "redirect:/admin/users";
     }
 
